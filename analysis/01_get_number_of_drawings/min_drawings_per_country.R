07be4ac9b5d348835e19998ba06d2be3732cf82c
Bin <binzheng@andrew.cmu.edu>
2020-11-12 12:30:11 -0500

    Number of drawings per country per category

--- analysis/01_get_number_of_drawings/min_drawings_per_country.R
+++ analysis/01_get_number_of_drawings/min_drawings_per_country.R
@@ -1,7 +1,7 @@
 # get number of drawings per country per category
 library(tidyverse)
 library(here)
-library(corpus) # for reading jaw ndjson files
+library(corpus) # for reading raw ndjson files
 
 CATEGORY_INFO_PATH <- here("data/raw/google_categories_coded.csv") #here("data\\raw\\google_categories_coded.csv")
 RAW_DRAWING_PATH <- file.path("C:/Users/binz7/Documents/.quickdraw") ## the ndjson files of the quickdraw package are downloaded on my laptop
@@ -17,15 +17,16 @@ target_categories <- categories %>%
 
 get_n_drawings_per_item <- function(category_name, file_path, out_path){
   name <- paste0(category_name, ".ndjson")
-  json_file <- file.path(file_path, name)
-  df <- read_ndjson(json_file)
+  json_path <- file.path(file_path, name)
+  df <- read_ndjson(json_path)
 
   data_for_one_item <- df %>%
     count(countrycode) %>%
     mutate(category = category_name)
 
-  print(category_name)
   write_csv(data_for_one_item, out_path, append = T)
+  print(category_name)
+  print(Sys.time())
 }
 
 walk(target_categories$category,
