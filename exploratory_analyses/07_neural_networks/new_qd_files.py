427d980fcf4167c47edd1c67e121c338a6cd6bdf
abalamur <38447587+abalamur@users.noreply.github.com>
2020-12-02 15:10:20 -0500

    Add files via upload

--- exploratory_analyses/07_neural_networks/new_qd_files.py
+++ exploratory_analyses/07_neural_networks/new_qd_files.py
@@ -42,20 +42,6 @@ print(df['bitmap'])
 
 # to convert back --> np.array(df['bitmap'][0], dtype=np.uint8)
 
-bread = np.c_[bread, np.zeros(len(bread))]
-def plot_samples(input_array, rows=1, cols=5, title=''):
-    fig, ax = plt.subplots(figsize=(cols,rows))
-    ax.axis('off')
-    plt.title(title)
-
-    for i in list(range(0, min(len(input_array),(rows*cols)) )):
-        a = fig.add_subplot(rows,cols,i+1)
-        imgplot = plt.imshow(input_array[i,:784].reshape((28,28)), cmap='gray_r', interpolation='nearest')
-        plt.xticks([])
-        plt.yticks([])
-
-#Plot arm samples
-plot_samples(bread, title='Sample arm drawings\n')[0]
 
 
 
@@ -71,17 +57,14 @@ plot_samples(bread, title='Sample arm drawings\n')[0]
 #use the few categories that we have human judgments for
 #start with one category like bread and train the model on bread and pass it the pairs that we
 # have human judgements for and get the similarity estimates for those pairs
-#train on 100000 drawings
-#get 100000 random sample of drawings (bread)
+#train on 10000 drawings
+#get 10000 random sample of drawings (bread)
 #look at each country for the bread drawing and take specific number of drawings
 #ask bin for a list of random sample of 10000 key ids
 #evaulate model and how good it is before we test it
 
 #print "no_of_pics, pixels_size"
-# print(arm.shape)
-# print(bicycle.shape)
-# print(book.shape)
-# print(paperClip.shape)
+
 print(bread.shape)
 
 # add a column with labels, 0=book, 1=bicycle, 2=book, 3=paperclip
@@ -89,38 +72,41 @@ print(bread.shape)
 # bicycle = np.c_[bicycle, np.ones(len(bicycle))]
 # book = np.c_[book, 2*np.ones(len(book))]
 # paperClip = np.c_[paperClip, 3*np.ones(len(paperClip))]
-bread = np.c_[bread, np.zeros(len(bread))]
+
+#bread = np.c_[bread, np.zeros(len(bread))]
+
 
 # merge the arrays, and split the features (X) and labels (y). Convert to float32 to save some memory.
 # X = np.concatenate((arm[:10000,:-1], bicycle[:10000,:-1], book[:10000,:-1], paperClip[:10000,:-1]), axis=0).astype('float32') # all columns but the last
 # y = np.concatenate((arm[:10000,-1], bicycle[:10000,-1], book[:10000,-1], paperClip[:10000,-1]), axis=0).astype('float32') # the last column
-X = np.concatenate(bread[:10000,:-1], axis=0).astype('float32')
-y = np.concatenate(bread[:10000,-1], axis=0).astype('float32')
+A = np.concatenate(bread[:10000], axis=0).astype('float32')
+b = np.concatenate(bread[:10000], axis=0).astype('float32')
 
-# train/test split (divide by 255 to obtain normalized values between 0 and 1)
-# Use a 50:50 split, training the models on 10'000 samples and thus have plenty of samples to spare for testing.
-X_train, X_test, y_train, y_test = train_test_split(X/255.,y,test_size=0.5,random_state=0)
-
-
-# one hot encode outputs
-y_train_cnn = np_utils.to_categorical(y_train) #converts class vector "y_train" to binary class matrix
-y_test_cnn = np_utils.to_categorical(y_test)
-num_classes = y_test_cnn.shape[1] #gets the number of cols (second element in tupule)
-
-# reshape to be [samples][pixels][width][height] --> became sample, width, height, pixels/channels
-X_train_cnn = X_train.reshape(X_train.shape[0], 28, 28, 1).astype('float32')
-X_test_cnn = X_test.reshape(X_test.shape[0], 28, 28, 1).astype('float32')
-s = X_train_cnn.shape
-print(s, num_classes)
-
-# define the CNN model
+# # train/test split (divide by 255 to obtain normalized values between 0 and 1)
+# # Use a 50:50 split, training the models on 10'000 samples and thus have plenty of samples to spare for testing.
+A_train, A_test, b_train, b_test = train_test_split(A/255.,b,test_size=0.5,random_state=0)
+#
+#
+# # one hot encode outputs
+b_train_cnn = np_utils.to_categorical(b_train) #converts class vector "y_train" to binary class matrix
+b_test_cnn = np_utils.to_categorical(b_test)
+num_classes = b_test_cnn.shape[1] #gets the number of cols (second element in tupule)
+#
+# # reshape to be [samples][pixels][width][height] --> became sample, width, height, pixels/channels
+#A_train = np.arange(3920000).reshape(20000, 784)
+A_train_cnn = A_train.reshape(A_train.shape[0], 28, 28, 1).astype('float32')
+A_test_cnn = A_test.reshape(A_test.shape[0], 28, 28, 1).astype('float32')
+s = A_train_cnn.shape
+# print(s, num_classes)
+#
+# # define the CNN model
 def cnn_model(): #780 in model summary
     # create model
     # provides the training and inference features on the model -->start
     model = Sequential()
 
     #onto the first layer, add the convolutional layer which uses dim of output(number of output filters,
-    #kernel size, padding so output has same width and height, input shape =rows,cols,channels since data_format = channels_last
+    #kernel size, padding so output has same width and height, input shape =rows,cols,channels since datac_format = channels_last
     #and activation function --> relu = rectified linear unit activation function
     model.add(Conv2D(30, (5, 5), padding="same", input_shape=(28, 28, 1), activation='relu')) #padding="same"
     print("conv weights =", model.get_weights())
@@ -160,13 +146,13 @@ model = cnn_model()
 #trains the model for a fixed number of epochs
 #validation_data =  Data on which to evaluate the loss and any model metrics at the end of each epoch.
 
-model.fit(X_train_cnn, y_train_cnn, validation_data=(X_test_cnn, y_test_cnn), epochs=1, batch_size=50) #changed from history =
+model.fit(A_train_cnn, b_train_cnn, validation_data=(A_test_cnn, b_test_cnn), epochs=1, batch_size=50) #changed from history =
 
 # Final evaluation of the model
 #returns the loss value and metric values for the model
 # x = input data, y = target data
 #verbose says how you want to see the training progress for each epoch
-scores = model.evaluate(X_test_cnn, y_test_cnn, verbose=0) #interpret
+scores = model.evaluate(A_test_cnn, b_test_cnn, verbose=0) #interpret
 print('Final CNN accuracy: ', scores[1]*100, "%")
 
 # Save weights
